b3a4d30ec620fef9f5c110ebcfdfc8e1d6e4b0e5
diegoxze35 <diegoalv1217@gmail.com>
2024-12-27 18:52:16 -0600

    Documented.

--- services/auth-service/src/main/kotlin/com/sgpttt/authservice/model/response/NotFound.kt
+++ services/auth-service/src/main/kotlin/com/sgpttt/authservice/model/response/NotFound.kt
@@ -1,3 +1,6 @@
 package com.sgpttt.authservice.model.response
 
+/**
+ * A Not found result that extends of [LoginResponse], it overrides [person] property with null
+ */
 data object NotFound: LoginResponse(person = null, result = "Person not found")
--- services/auth-service/src/main/kotlin/com/sgpttt/authservice/model/response/WrongPassword.kt
+++ services/auth-service/src/main/kotlin/com/sgpttt/authservice/model/response/WrongPassword.kt
@@ -2,4 +2,8 @@ package com.sgpttt.authservice.model.response
 
 import com.sgpttt.authservice.model.domain.PersonDTO
 
+/**
+ * If the person who is trying to make login wrote its password incorrectly an object of this class is returned,
+ * it overrides [person] property with the person witch was found by its email
+ */
 data class WrongPassword(override val person: PersonDTO): LoginResponse(person, result = "Wrong password")
