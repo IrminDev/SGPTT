81419b5ab5b7d63a91be720f857f67bce1997424
Emiliano <ediazh1900@alumno.ipn.mx>
2025-01-02 11:14:23 -0600

    Dockerfile

--- services/administration-service/src/main/java/sgptt/adminsvc/repository/ProfessorRepository.java
+++
@@ -1,9 +0,0 @@
-package sgptt.adminsvc.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import sgptt.adminsvc.entity.Professor;
-
-@Repository
-public interface ProfessorRepository extends JpaRepository<Professor, Long> {
-}
--- services/administration-service/src/main/java/sgptt/adminsvc/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package sgptt.adminsvc.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import sgptt.adminsvc.entity.Student;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
